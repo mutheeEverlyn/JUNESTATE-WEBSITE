ace5ca1791293cfad0772efd17e08b3ce6d202b0
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-12-01 19:54:03 +0300

    added files

---
+++ src/features/payments/PaymentsApi.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+export interface TPayments{
+    checkouturl:string;
+
+}
+export const PaymentsAPI = createApi({
+  reducerPath: 'paymentsAPI',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:8000/',
+  }),
+  endpoints: (builder) => ({
+    createPayments: builder.mutation<TPayments,{booking_id:number;amount:number}>({
+      query: ({booking_id,amount}) => ({
+        url: 'create-checkout-session',
+        method: 'POST',
+        body: {booking_id,amount,success_url:'https://vehicle-api-5bvc.onrender.com/payment-success',cancel_url:'https://vehicle-api-5bvc.onrender.com/payment-cancel'},
+      }),
+    }), 
+  }),
+});
+export const {
+  useCreatePaymentsMutation
+}:any = PaymentsAPI;
